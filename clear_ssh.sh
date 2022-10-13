acc69ca8702d797e31ce80ec77dd0fe110e8e055
xDruid <savelyev.eg@gmail.com>
2022-10-13 13:41:07 +0700

    clear known-hosts for vagrant

---
+++ clear_ssh.sh
@@ -0,0 +1,3 @@
+ssh-keygen -f "/home/$1/.ssh/known_hosts" -R "192.168.10.20"
+ssh-keygen -f "/home/$1/.ssh/known_hosts" -R "192.168.10.21"
+ssh-keygen -f "/home/$1/.ssh/known_hosts" -R "192.168.10.22"
